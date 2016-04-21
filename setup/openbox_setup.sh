d5d83b5d1dcbe1084d29e12d10101314dc9fad22
xaviervalarino <xavier@valarino.com>
2016-04-21 14:29:26 -0700

    Check if .config and openbox dir exists in setup
    The setup script for openbox now checks to see if the
    ~/.config and ~/.config/openbox directories exist.

--- setup/openbox_setup.sh
+++ setup/openbox_setup.sh
@@ -6,11 +6,24 @@ OPENBOX CONFIGURATION
 ---------------------
 
 EOT
+config="$HOME/.config/"
+openbox="${config}openbox/"
+# create config and openbox dir
+if [ ! -d $config ]; then
+  echo "Creating config directory at ${config}"
+  mkdir $config
+fi
+if [ ! -d $openbox ]; then
+  echo "Creating openbox directory at ${openbox}"
+  mkdir $openbox
+fi
 
 for file in autostart menu.xml rc.xml
 do
-  configFile="$HOME/.config/openbox/$file"
-  if [ -e "$configFile" ]; then
+  configFile="$openbox$file"
+
+  # check for old file
+  if [ -e $configFile ]; then
     echo "(!) ${file} exists, renaming ${file}.old"
     mv $configFile ${configFile}.old
   fi
