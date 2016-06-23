6f837c48a06b70c71fdaa24f51852a1b5f999cd3
Xavier Valarino <xvalarino@gmail.com>
2016-06-22 22:14:48 -0700

    Split Kwm config into main file and rules
    Rules control how specific application windows behave-whether they
    should float, be in a specific window, or space.

--- setup/kwm.sh
+++ setup/kwm.sh
@@ -2,4 +2,9 @@
 # install KWM tiling windows manager & add to launchtl startup
 brew install/binary/kwm
 brew services start homebrew/binary/kwm
-ln -s /Users/Xavier/.dotfiles/kwmrc /Users/Xavier/.kwm/
+
+dotfiles="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+
+mkdir  -p $HOME/.kwm
+ln -s $dotfiles/kwm/kwmrc $HOME/.kwm/kwmrc
+ln -s $dotfiles/kwm/rules $HOME/.kwm/rules
