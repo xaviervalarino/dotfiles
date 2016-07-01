0ce27b82c0dec571c1515b4e7f2745115e646c17
Xavier Valarino <xvalarino@gmail.com>
2016-06-30 21:57:33 -0700

    Update Kwm setup script to link directory
    The setup script links the `.kwm` directory instead of the individual
    files. So, if in the future, I decide to `include` more files to
    `kwmrc`, I will not have to manually update the script to symlink them.

--- setup/kwm.sh
+++ setup/kwm.sh
@@ -1,18 +1,33 @@
 #! /bin/bash
+
 # install KWM tiling windows manager & add to launchtl startup
 brew install homebrew/binary/kwm
 
 dotfiles="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
-mkdir  -p $HOME/.kwm
 
 # TODO create a bash function that takes a filename and, if it exists,
-# prompts user to either raname it to old and write new symlink, or abort,
-# or use ln --backup --suffix .old
+#      prompts user to either rename it to old and write new symlink, or abort,
+#      or use ln --backup --suffix .old
+# Use function in all setup scripts
 
-ln -s $dotfiles/kwm/kwmrc $HOME/.kwm/kwmrc
-ln -s $dotfiles/kwm/rules $HOME/.kwm/rules
-echo Kwm config files linked to $HOME/.kwm
+if [ -d $HOME/.kwm ]; then
+	yellow='\033[1;33m'
+	red='\033[1;31m'
+	reset='\033[0m' # no color
+
+	echo -e ${yellow}Warning:${reset} Existing Kwm directory found
+    if [ -d $HOME/.kwm.old ]; then
+		echo -e ${yellow}Error:${reset} \'.kwm\' and \'.kwm.old\' exist
+		echo You must remove these directories manually before rerunning this script
+		exit 130
+    fi
+	echo It has been renamed \'.kwm.old\'
+    mv $HOME/.kwm $HOME/.kwm.old
+fi
+
+ln -bsS .old $dotfiles/kwm $HOME/.kwm
+echo Kwm config directory linked to $HOME/.kwm
+
 echo Starting Kwm
-
 brew services start homebrew/binary/kwm
