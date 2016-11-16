72f59c3acbc77b82ad17792d97deeae080092e59
xaviervalarino <xavier@valarino.com>
2016-11-15 16:16:16 -0800

    Dynamically look up dotfiles dir when linking gitconfig

--- setup/gitconfig.sh
+++ setup/gitconfig.sh
@@ -1,1 +1,7 @@
-ln -s $HOME/.dotfiles/gitconfig $HOME/.gitconfig
+#! /bin/bash
+
+dotfiles="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+echo "Linking '.gitconfig' to $HOME"
+ln -s $dotfiles $HOME/.gitconfig
+
+npm i -g diff-so-fancy
