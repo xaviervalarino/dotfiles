5c5cb22174a3f1f64ae6d31cf71b90af493623ca
xaviervalarino <xavier@valarino.com>
2016-11-15 16:26:57 -0800

    Use the script dir when looking for util function

--- setup/kwm.sh
+++ setup/kwm.sh
@@ -1,10 +1,20 @@
 #! /bin/bash
-source ./util_functions.sh
+
+cat<<"EOT"
+---------------------------
+KWM INSTALL & CONFIGURATION
+---------------------------
+
+EOT
+
+currentDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+
+source "${currentDir}/setup/util_functions.sh"
 
 # install KWM tiling windows manager & add to launchtl startup
-brew install homebrew/binary/kwm
+brew install koekeishiya/kwm/kwm
 
-symlink_dir kwm $HOME
+symlink_dir .kwm $HOME
 
 echo Starting Kwm
-brew services start homebrew/binary/kwm
+brew services start koekeishiya/kwm/kwm
