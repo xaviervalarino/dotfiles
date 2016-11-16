24cfa8cee2a11b80ba65370a74cee9f23c7e2fbb
xaviervalarino <xavier@valarino.com>
2016-11-15 16:20:09 -0800

    Add messaging and wont overwrite existing file

--- setup/gitconfig.sh
+++ setup/gitconfig.sh
@@ -1,7 +1,21 @@
 #! /bin/bash
 
+cat<<"EOT"
+-----------------
+GIT CONFIGURATION
+-----------------
+
+EOT
+# Don't overwrite existing gitconfig
+gitconfig=${HOME}/.gitconfig
+if [ -f $gitconfig ]; then
+  echo "(!) Your old .gitconfig has been renamed .gitconfig.old"
+  mv $gitconfig "$HOME/.gitconfig.old"
+fi
+
+# SymLink gitconfig to home dir
 dotfiles="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 echo "Linking '.gitconfig' to $HOME"
-ln -s $dotfiles $HOME/.gitconfig
+ln -s -f ${dotfiles}/gitconfig $gitconfig
 
 npm i -g diff-so-fancy
