9807c66a039d93a9cacd209e37ea75b1e90a4a15
xaviervalarino <xavier@valarino.com>
2018-03-28 23:14:16 -0700

    Prune unused/obsolete configs

--- kwm/.kwm/scripts/check-display.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-external_monitor=$(system_profiler SPDisplaysDataType | grep -ic 23EA63)
-[ $external_monitor -eq 1 ] && $1
--- kwm/.kwm/scripts/pause-kwm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# This script is triggered through a hotkey using `khd`
-
-if [ $(pidof kwm) ]; then
-    brew services stop kwm
-else
-    brew services restart kwm
-fi
