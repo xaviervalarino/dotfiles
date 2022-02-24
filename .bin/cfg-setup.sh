80231a5c4c1d9d8fac31f984f6a6f8af35ca4181
Xavier Valarino <xavier@valarino.com>
2022-02-24 01:25:11 -0800

    Complete dotfiles merge
    Moving back  to using `stow` to manage dotfiles over a git bare repo.

--- .bin/cfg-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-# Setup for storing doftiles in a bare Git repository
-# https://www.atlassian.com/git/tutorials/dotfiles
-
-git clone --bare https://github.com/xaviervalarino/dots "$HOME"/.cfg
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --working-tree=$HOME'
-config config --local status.showUntrackedFiles no
