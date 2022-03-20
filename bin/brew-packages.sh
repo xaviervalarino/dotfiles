80d7ebfe4572520c87acc7b3e2a4a8ed5a92bd06
Xavier Valarino <xvalarino@gmail.com>
2022-03-20 14:01:38 -0700

    Brew: update installed packaged

--- bin/brew-packages.sh
+++ bin/brew-packages.sh
@@ -1,36 +1,41 @@
 #!/usr/bin/env sh
 
 # Core
+# `brew leaves --installed-on-request`
 CORE=(
   bat
-  fzf  
-  git 
-  git-delta 
-  m-cli 
-  node 
-  nvm 
+  fd
+  fzf
+  git
+  git-delta
+  lua-language-server
+  luajit
+  m-cli
+  n
+  neovim
   pidof
-  tldr 
-  trash 
-  vim 
+  ripgrep
   shellcheck
+  stow
   the_silver_searcher
+  tldr
+  trash
   tree
   utimer
   zsh-completions
 )
 
 CASKS=(
+  font-iosevka
   hammerspoon 
   iterm2 
   karabiner-elements 
-  logitech-options 
 )
 
 printf "%s\n" "Installing core packages..."
 brew install ${CORE[@]}
 
 printf "%s\n" "Installing casks..."
-brew install ${CASKS[@]}
+brew install --cask ${CASKS[@]}
 
 
