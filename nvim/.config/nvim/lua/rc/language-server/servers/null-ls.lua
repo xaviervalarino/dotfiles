2a9e796db2cc6386208d24173f4e410f91a65219
Xavier Valarino <xvalarino@gmail.com>
2022-03-20 16:40:23 -0700

    Add global eslint and turn on neovim linter

--- nvim/.config/nvim/lua/rc/language-server/servers/null-ls.lua
+++ nvim/.config/nvim/lua/rc/language-server/servers/null-ls.lua
@@ -4,8 +4,8 @@ if not status_ok then return false end
 null_ls.setup {
   debug = true,
   sources = {
-    -- null_ls.builtins.diagnostics.eslint_d,
-    -- null_ls.builtins.code_actions.eslint_d,
+    null_ls.builtins.diagnostics.eslint_d,
+    null_ls.builtins.code_actions.eslint_d,
     -- null_ls.builtins.formatting.prettier.with {
     null_ls.builtins.formatting.prettier_d_slim.with {
       extra_args = { '--single-quote', '--jsx-single-quote' }
