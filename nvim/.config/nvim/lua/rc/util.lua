935d48ddffa31714ab9aa521b91da8b1277a5745
Xavier Valarino <xvalarino@gmail.com>
2022-03-30 17:34:53 -0700

    Nvim: remove unused `compose` utility function

--- nvim/.config/nvim/lua/rc/util.lua
+++ nvim/.config/nvim/lua/rc/util.lua
@@ -17,18 +17,4 @@ function M.create_buf_keymap(bufnr)
   end
 end
 
--- compose specific values from two tables
--- composer is a table of keys you want to combine from the two tables
--- and the values are a set of callbacks that return the composed values
-function M.compose_values(t1, t2, composer)
-  local composed = {}
-  for k, v in pairs(composer) do
-    if t1[k] and t2[k] then
-      print('composing', k)
-      composed[k] = v(t1, t2)
-    end
-  end
-  return composed
-end
-
 return M
