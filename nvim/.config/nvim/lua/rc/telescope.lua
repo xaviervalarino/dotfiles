8d4b48975b7b378fc76b7093d88b0971d8ef9e67
Xavier Valarino <xvalarino@gmail.com>
2022-05-22 16:18:26 -0500

    Nvim: setup telescope plugin
    Still need to add native FZF
    More or less copied kickstart.nvim: https://github.com/nvim-lua/kickstart.nvim/blob/fd7f05d872092673ef6a883f72edbf859d268a2e/init.lua#L116

---
+++ nvim/.config/nvim/lua/rc/telescope.lua
@@ -0,0 +1,30 @@
+local status_ok, telescope = pcall(require, 'telescope')
+if not status_ok then
+  return
+end
+
+local nmap = require('rc.util').create_keymaps 'n'
+local builtin = require 'telescope.builtin'
+
+telescope.setup {
+  defaults = {
+    mappings = {
+      i = {
+        ['<C-u>'] = false,
+        ['<C-d>'] = false,
+      },
+    },
+  },
+}
+
+nmap('<leader><space>', builtin.buffers)
+nmap('<leader>sf', builtin.find_files)
+nmap('<leader>sb', builtin.current_buffer_fuzzy_find)
+nmap('<leader>sh', builtin.help_tags)
+nmap('<leader>st', builtin.tags)
+nmap('<leader>sd', builtin.grep_string)
+nmap('<leader>sp', builtin.live_grep)
+nmap('<leader>so', function()
+  builtin.tags { only_current_buffer = true }
+end)
+nmap('<leader>?', builtin.oldfiles)
