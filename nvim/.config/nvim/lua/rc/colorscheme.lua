1296d5ede0d657338b7c71d1d78373320fb0b7dc
Xavier Valarino <xvalarino@gmail.com>
2022-06-12 13:31:04 -0500

    Nvim: fix float border and background color

--- nvim/.config/nvim/lua/rc/colorscheme.lua
+++ nvim/.config/nvim/lua/rc/colorscheme.lua
@@ -1,8 +1,27 @@
+local catppuccin = require 'catppuccin'
+local colors = require('catppuccin.api.colors').get_colors()
+
 local _, _, color = vim.env.ITERM_PROFILE:find '-(%a+)'
 for _, v in ipairs { 'latte', 'frappe', 'macchiato', 'mocha' } do
   if color == v then
     vim.g.catppuccin_flavour = color
   end
 end
+
+catppuccin.remap {
+  FloatBorder = {
+    fg = colors.sapphire,
+    bg = colors.base,
+  },
+  FloatShadow = {
+    bg = colors.base,
+  },
+  FloatShadowThrough = {
+    bg = colors.base,
+  },
+  NormalFloat = {
+    bg = colors.base,
+  },
+}
 
 vim.cmd [[colorscheme catppuccin]]
