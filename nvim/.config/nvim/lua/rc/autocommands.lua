199facd299b0711b9cd374be64880b7e64cff0ee
Xavier Valarino <xvalarino@gmail.com>
2022-07-29 16:41:07 -0700

    Nvim: move all autocommands to lua

--- nvim/.config/nvim/lua/rc/autocommands.lua
+++ nvim/.config/nvim/lua/rc/autocommands.lua
@@ -7,3 +7,28 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+-- Turn off relative line numbers for inactive windows
+local bkg_numbers = vim.api.nvim_create_augroup('BkgNumbers', { clear = true })
+vim.api.nvim_create_autocmd('WinEnter', {
+  group = bkg_numbers,
+  pattern = '*',
+  callback = function()
+    vim.opt_local.relativenumber = true
+  end,
+})
+vim.api.nvim_create_autocmd('WinLeave', {
+  group = bkg_numbers,
+  pattern = '*',
+  callback = function()
+    vim.opt_local.relativenumber = false
+  end,
+})
+
+-- not sure if I need this 
+-- local preview_nonumbers = vim.api.nvim_create_augroup('PreviewNoNumbers', { clear = true })
+-- vim.api.nvim_create_autocmd('WinEnter', {
+--   group = preview_nonumbers,
+--   pattern = '*',
+--   command = 'if &previewwindow | setlocal nonumber | endif',
+-- })
