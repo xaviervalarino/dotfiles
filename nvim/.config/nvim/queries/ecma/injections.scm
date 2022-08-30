28b62a0c8c34de4b844afd4e3fd12283f28bac74
Xavier Valarino <xvalarino@gmail.com>
2022-08-30 14:29:51 -0700

    Nvim: format treesitter query

--- nvim/.config/nvim/queries/ecma/injections.scm
+++ nvim/.config/nvim/queries/ecma/injections.scm
@@ -1,6 +1,5 @@
 ; Highlight HTML/CSS used in template string literals
 ; matches opening and closing tags
-(
-  (template_string) @html
+((template_string) @html
   (#match? @html "\\<.*\\>.*\\</.*\\>")
-)
+  (#offset! @html 0 1 0 -1))
