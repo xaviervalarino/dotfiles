98dddbdf5261768c311001392ce3a36e1fbcc3c7
Xavier Valarino <xvalarino@gmail.com>
2022-08-30 17:49:41 -0700

    Nvim: move LSP settings to LSP config file

--- nvim/.config/nvim/plugin/diagnostics.lua
+++ nvim/.config/nvim/plugin/diagnostics.lua
@@ -40,6 +40,3 @@ vim.diagnostic.config {
     prefix = '',
   },
 }
-
-vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { border = 'rounded' })
-vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, { borders = 'rounded' })
