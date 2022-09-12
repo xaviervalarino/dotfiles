7eb9fe11c88169c3dea1bebec1547055d688b351
Xavier Valarino <xvalarino@gmail.com>
2022-09-12 15:50:37 -0700

    HS: make indicator work with external monitors

--- hammerspoon/.hammerspoon/focused-window-indicator.lua
+++ hammerspoon/.hammerspoon/focused-window-indicator.lua
@@ -1,25 +1,44 @@
+
 local M = {}
 
-M.canvas = (function()
-  local max = hs.screen.mainScreen():fullFrame()
-  local canvas = hs.canvas.new { x = max.x, y = max.y, h = max.h, w = max.w }
-  canvas:appendElements {
-    type = 'rectangle',
-    action = 'fill',
-    fillColor = { red = 0, green = 0, blue = 0, alpha = 0.6 },
-    frame = { x = max.x, y = max.y, h = max.h, w = max.w },
-  }
-  canvas:level 'normal'
-  return canvas
-end)()
+local function create_canvases()
+  local canvases = {}
+  for _, screen in ipairs(hs.screen.allScreens()) do
+    local max = screen:fullFrame()
+    local canvas = hs.canvas.new { x = max.x, y = max.y, h = max.h, w = max.w }
+    local relative = screen:absoluteToLocal(max)
+    canvas:appendElements {
+      type = 'rectangle',
+      action = 'fill',
+      fillColor = { red = 0, green = 0, blue = 0, alpha = 0.6 },
+      frame = { x = relative.x, y = relative.y, h = relative.h, w = relative.w },
+    }
+    canvas:level 'normal'
+    canvases[screen:name()] = canvas
+  end
+  return canvases
+end
+M.canvases = create_canvases()
+
+Screen_watcher = hs.screen.watcher.new(function()
+  M.canvases = create_canvases()
+end)
+Screen_watcher:start()
 
 M.on = function()
+  -- TODO: Hammerspoon console does not get recognized as focusedWindow
   hs.window.focusedWindow():raise()
-  M.canvas:show(0.4)
+  for _, canvas in pairs(M.canvases) do
+    print(hs.inspect(canvas.hide))
+    canvas:show(0.4)
+  end
 end
 
 M.off = function()
-  M.canvas:hide(0.4)
+  hs.window.focusedWindow():raise()
+  for _, canvas in pairs(M.canvases) do
+    canvas:hide(0.4)
+  end
 end
 
 return M
