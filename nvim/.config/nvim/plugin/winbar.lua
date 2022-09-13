32458ceba7b1a3e3051137fd8e0b4fabfa1c59d7
Xavier Valarino <xvalarino@gmail.com>
2022-09-13 13:07:45 -0700

    Nvim: update Winbar
    * Center in window
    * Add file modified state
    * Use autocmd to exclude winbar from certain filetypes

--- nvim/.config/nvim/plugin/winbar.lua
+++ nvim/.config/nvim/plugin/winbar.lua
@@ -1,11 +1,29 @@
 function _G.Winbar()
-  local file_name, file_ext = vim.fn.expand '%:t', vim.fn.expand '%:e'
-  local icon = require('nvim-web-devicons').get_icon(file_name, file_ext, { default = true }) .. ' '
+  local file_name = vim.fn.expand '%:t'
+  local file_ext =  vim.fn.expand '%:e'
+  local icon = require('nvim-web-devicons').get_icon(file_name, file_ext, { default = true })
+
+  if not file_name then
+    return ' '
+  end
+
   return table.concat {
     '%#@text# ',
+    '%=',
     icon,
+    '  ', -- space for icon
     '%t',
+    ' %m',
+    '%=',
   }
 end
 
 vim.o.winbar = '%{%v:lua.Winbar()%}'
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'alpha', 'netrw' },
+  group = vim.api.nvim_create_augroup('winbar', { clear = true }),
+  callback = function()
+    vim.wo.winbar = ' '
+  end,
+})
