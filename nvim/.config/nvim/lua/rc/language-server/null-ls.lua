091d57a537598f44ae90e318d25b5b44c5d7e46c
Xavier Valarino <xvalarino@gmail.com>
2023-02-05 20:18:48 -0500

    Nvim: remove `xo` from `null-ls`

--- nvim/.config/nvim/lua/rc/language-server/null-ls.lua
+++ nvim/.config/nvim/lua/rc/language-server/null-ls.lua
@@ -4,7 +4,6 @@ if not status_ok then
 end
 
 local diagnostics = null_ls.builtins.diagnostics
-local code_actions = null_ls.builtins.code_actions
 local formatting = null_ls.builtins.formatting
 
 local M = {}
@@ -14,8 +13,6 @@ function M.setup(opts)
     debug = true,
     on_attach = opts.on_attach,
     sources = {
-      code_actions.xo,
-      diagnostics.xo,
       diagnostics.shellcheck,
       formatting.stylua,
       formatting.prettier,
