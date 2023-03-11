f61929d39d388f79d58e0dd4d80bd646779b94ea
Xavier Valarino <xvalarino@gmail.com>
2023-03-11 15:37:12 -0800

    Setup: icon font is now available in brew cask

--- setup/configs.sh
+++ setup/configs.sh
@@ -39,11 +39,3 @@ cargo install stylua
 stow -Rv nvim
 # TODO: set up nvim
 # nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
-
-# Add secondary icon font used in iTerm and Nvim
-# Note: update this to `brew --cask install font-*` when this ticket is closed:
-# https://github.com/ryanoasis/nerd-fonts/issues/479
-# TODO: not working
-# curl --LJO https://github.com/ryanoasis/nerd-fonts/blob/master/src/glyphs/Symbols-2048-em%20Nerd%20Font%20Complete.ttf\
-  # --output "$HOME/Library/Fonts/Symbols-2048-em Nerd Font Complete.ttf"
-open https://github.com/ryanoasis/nerd-fonts/blob/master/src/glyphs/Symbols-2048-em%20Nerd%20Font%20Complete.ttf
