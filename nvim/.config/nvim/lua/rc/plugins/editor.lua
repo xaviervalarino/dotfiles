d9e6efe14dfd50064119182a78768343d6491444
Xavier Valarino <xvalarino@gmail.com>
2023-06-11 15:23:29 -0700

    Reorganize plugins into `editor` and `ui`

---
+++ nvim/.config/nvim/lua/rc/plugins/editor.lua
@@ -0,0 +1,50 @@
+local function cb(modname, method, ...)
+  local args = { ... }
+  return function()
+    require(modname)[method](unpack(args))
+  end
+end
+
+return {
+  {
+    'windwp/nvim-autopairs',
+    config = true,
+    keys = (function()
+      local t = {}
+      for _, key in ipairs { '{', '}', '[', ']', "'", '"', '`' } do
+        table.insert(t, { key, mode = 'i' })
+      end
+      return t
+    end)(),
+  },
+  { 'kylechui/nvim-surround', event = 'VeryLazy', config = true },
+  { 'tpope/vim-repeat', lazy = true },
+  { 'tpope/vim-fugitive', cmd = { 'G', 'Git' } },
+  { 'godlygeek/tabular', cmd = { 'Tab', 'Tabularize' } },
+
+  {
+    'ThePrimeagen/harpoon',
+    config = true,
+    keys = {
+      { '<leader>pa', cb('harpoon.mark', 'add_file'), desc = 'Harpoon: add file to list' },
+      { '<leader>l', cb('harpoon.ui', 'toggle_quick_menu'), desc = 'Harpoon: list all files' },
+      { '<leader>1', cb('harpoon.ui', 'nav_file', 1), desc = 'Harpoon: Go to 1st file' },
+      { '<leader>2', cb('harpoon.ui', 'nav_file', 2), desc = 'Harpoon: Go to 2nd file' },
+      { '<leader>3', cb('harpoon.ui', 'nav_file', 3), desc = 'Harpoon: Go to 3rd file' },
+      { '<leader>4', cb('harpoon.ui', 'nav_file', 4), desc = 'Harpoon: Go to 4th file' },
+    },
+  },
+  { 'smjonas/inc-rename.nvim', cmd = 'IncRename', config = true },
+  {
+    'numToStr/Comment.nvim',
+    event = { 'BufReadPost', 'BufNewFile' },
+    dependencies = { 'JoosepAlviste/nvim-ts-context-commentstring' },
+    config = function()
+      require('Comment').setup {
+        pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+      }
+    end,
+  },
+  { 'gpanders/editorconfig.nvim', event = { 'BufReadPost', 'BufNewFile' } },
+  { 'tiagovla/scope.nvim', event = { 'TabEnter', 'TabLeave' } },
+}
---
+++ nvim/.config/nvim/lua/rc/plugins/ui.lua
@@ -0,0 +1,112 @@
+return {
+  {
+    'norcalli/nvim-colorizer.lua',
+    event = { 'BufReadPost', 'BufNewFile' },
+    opts = {
+      '*',
+      css = { css = true },
+    },
+  },
+  {
+    'folke/which-key.nvim',
+    event = 'VeryLazy',
+    opts = {
+      plugins = {
+        spelling = {
+          enabled = true,
+        },
+      },
+      window = {
+        border = require('rc.util').float_win_style.border,
+        margin = { 1, 5, 2, 7 },
+      },
+    },
+  },
+  { 'kyazdani42/nvim-web-devicons', lazy = true },
+  {
+    'folke/zen-mode.nvim',
+    cmd = 'ZenMode',
+    opts = {
+      window = {
+        backdrop = 1,
+        width = 80,
+      },
+      plugins = {
+        -- requires `allow_remote_control` and 'listen_on' to be set
+        kitty = {
+          -- enabled = true,
+          enabled = false, -- doesn't return the font size on close, see: https://github.com/folke/zen-mode.nvim/issues/87
+          font = '+1',
+        },
+      },
+    },
+  },
+  {
+    'luukvbaal/statuscol.nvim',
+    event = { 'BufReadPre', 'BufNewFile' },
+    dependencies = { 'lewis6991/gitsigns.nvim' },
+    opts = function()
+      local builtin = require 'statuscol.builtin'
+
+      return {
+        relculright = true,
+        segments = {
+          {
+            text = { ' ', builtin.lnumfunc, ' ' },
+            click = 'v:lua.ScLa',
+          },
+          {
+            sign = {
+              name = { 'GitSigns' },
+              maxwidth = 1,
+              colwidth = 1,
+              fillchar = '▏',
+            },
+            click = 'v:lua.ScSa',
+          },
+        },
+        ft_ignore = {
+          'help',
+          'vim',
+          'alpha',
+          'lazy',
+          'toggleterm',
+        },
+      }
+    end,
+  },
+  {
+    'lukas-reineke/indent-blankline.nvim',
+    event = { 'BufReadPost', 'BufNewFile' },
+    opts = {
+      use_treesitter = true,
+      -- show_trailing_blankline_indent = false,
+      -- show_end_of_line = true,
+      buftype_exclude = { 'terminal', 'help', 'nofile' },
+      show_current_context = true,
+      -- show_current_context_start = true,
+      show_first_indent_level = false,
+      context_patterns = {
+        'class',
+        'function',
+        'method',
+        'element',
+        '^if',
+        '^while',
+        '^for',
+        '^object',
+        '^table',
+        'block',
+        'arguments',
+      },
+    },
+  },
+  {
+    'goolord/alpha-nvim',
+    event = 'VimEnter',
+    dependencies = { 'kyazdani42/nvim-web-devicons' },
+    config = function()
+      require('alpha').setup(require('alpha.themes.startify').config)
+    end,
+  },
+}
