ae52e166eeadce527eeb95f8777228d382bd3c79
Xavier Valarino <xvalarino@gmail.com>
2024-07-24 16:39:40 -0700

    Nvim: Use smart path display in Telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -49,11 +49,12 @@ return { -- Fuzzy Finder (files, lsp, etc)
       -- You can put your default mappings / updates / etc. in here
       --  All the info you're looking for is in `:help telescope.setup()`
       --
-      -- defaults = {
-      --   mappings = {
-      --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-      --   },
-      -- },
+      defaults = {
+        path_display = { 'smart' },
+        --   mappings = {
+        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+        --   },
+      },
       -- pickers = {}
       extensions = {
         ['ui-select'] = {
