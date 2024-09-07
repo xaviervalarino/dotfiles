de3dd188508dd8d470bc5e2cf5a23518de5bf9ac
Xavier Valarino <xvalarino@gmail.com>
2024-09-06 17:34:19 -0700

    Nvim: add Aerial code outline plugin

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -313,4 +313,24 @@ return {
       }
     end,
   },
+  {
+    'stevearc/aerial.nvim',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+    },
+    config = function()
+      require('aerial').setup {
+        layout = {
+          width = 0.2,
+          default_direction = 'prefer_left',
+        },
+        attach_mode = 'global',
+        on_attach = function(bufnr)
+          vim.keymap.set('n', '{', '<cmd>AerialPrev<CR>', { buffer = bufnr })
+          vim.keymap.set('n', '}', '<cmd>AerialNext<CR>', { buffer = bufnr })
+        end,
+      }
+      vim.keymap.set('n', '<leader>a', '<cmd>AerialToggle!<CR>', { desc = 'Open Code Outline' })
+    end,
+  },
 }
