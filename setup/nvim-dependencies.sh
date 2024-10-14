6a0f81d251472099d5e06a2df525b1b0aa64be96
Xavier Valarino <xvalarino@gmail.com>
2024-10-14 11:53:46 -0700

    Add script to install nvim LSP and format dependencies

---
+++ setup/nvim-dependencies.sh
@@ -0,0 +1,11 @@
+#!usr/bin/env sh
+
+brew install lua-language-server
+
+npm -g install \
+  @fsouza/prettierd \
+  @vtsls/language-server \
+  cssmodules-language-server \
+  graphql-language-service-cli \
+  some-sass-language-server \
+  vscode-langservers-extracted 
