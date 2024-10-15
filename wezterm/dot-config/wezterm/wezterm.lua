c6d0420dab70eb55fbd83cb742019d4980690f1d
Xavier Valarino <xvalarino@gmail.com>
2024-10-15 13:06:22 -0700

    Formatting with stylua

--- wezterm/dot-config/wezterm/wezterm.lua
+++ wezterm/dot-config/wezterm/wezterm.lua
@@ -1,16 +1,16 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = {}
 
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+    config = wezterm.config_builder()
 end
 
-config.color_scheme = 'rose-pine'
+config.color_scheme = "rose-pine"
 -- config.font = wezterm.font 'Iosevka Term'
 config.font_size = 15
 -- config.use_ime = false
 
-config.window_decorations = 'RESIZE'
+config.window_decorations = "RESIZE"
 config.use_resize_increments = true
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
@@ -20,49 +20,49 @@ config.use_fancy_tab_bar = false
 -- TODO: set this up so that it switches with term theme
 config.colors = {
     -- Set the Rose Pine theme selection colors
-  selection_fg = '#e0def4',  -- Use 'text' color for the selection foreground
-  selection_bg = '#26233a',  -- Use 'overlay' color for the selection background
+    selection_fg = "#e0def4", -- Use 'text' color for the selection foreground
+    selection_bg = "#26233a", -- Use 'overlay' color for the selection background
 
-  -- Set the cursor colors to fit the theme
-  cursor_fg = '#191724',  -- Use 'base' color for cursor foreground
-  cursor_bg = '#e0def4',  -- Use 'text' color for cursor background
+    -- Set the cursor colors to fit the theme
+    cursor_fg = "#191724", -- Use 'base' color for cursor foreground
+    cursor_bg = "#e0def4", -- Use 'text' color for cursor background
 }
 
 local default_window_padding = {
-  left = '2cell',
-  right = '2cell',
-  top = '2cell',
-  bottom = '2cell',
+    left = "2cell",
+    right = "2cell",
+    top = "2cell",
+    bottom = "2cell",
 }
 
 local nvim_window_padding = {
-  left = '0cell',
-  right = '0cell',
-  top = '0cell',
-  bottom = '0cell',
+    left = "0cell",
+    right = "0cell",
+    top = "0cell",
+    bottom = "0cell",
 }
 
 config.window_padding = default_window_padding
 
 local function set_padding(window, enter_event)
-  local overrides = window:get_config_overrides() or {}
+    local overrides = window:get_config_overrides() or {}
 
-  if enter_event then
-    overrides.window_padding = nvim_window_padding
-  else
-    overrides.window_padding = nil
-  end
+    if enter_event then
+        overrides.window_padding = nvim_window_padding
+    else
+        overrides.window_padding = nil
+    end
 
-  window:set_config_overrides(overrides)
+    window:set_config_overrides(overrides)
 end
 
-wezterm.on('user-var-changed', function(window, _, name, value)
-  if name == 'NVIM_EVENT' then
-    local enter_event = value == 'IN'
-    set_padding(window, enter_event)
-  end
+wezterm.on("user-var-changed", function(window, _, name, value)
+    if name == "NVIM_EVENT" then
+        local enter_event = value == "IN"
+        set_padding(window, enter_event)
+    end
 
-  return false
+    return false
 end)
 
 return config
