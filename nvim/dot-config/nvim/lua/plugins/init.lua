0030d984c25f5b6f9aeb03f86fadd11b03bdc401
Xavier Valarino <xvalarino@gmail.com>
2025-06-07 15:22:28 -0700

    Nvim: add mini.hipatterns to highlight colors in code

--- nvim/dot-config/nvim/lua/plugins/init.lua
+++ nvim/dot-config/nvim/lua/plugins/init.lua
@@ -61,7 +61,25 @@ return {
             require("mini.git").setup()
         end,
     },
+    {
+        "echasnovski/mini.hipatterns",
+        enabled = not vim.g.vscode,
+        version = false,
+        config = function()
+            local hipatterns = require("mini.hipatterns")
+            hipatterns.setup({
+                highlighters = {
+                    fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+                    hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
+                    todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+                    note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
 
+                    -- Highlight hex color strings (`#rrggbb`) using that color
+                    hex_color = hipatterns.gen_highlighter.hex_color(),
+                },
+            })
+        end,
+    },
     {
         "echasnovski/mini.animate",
         version = false,
