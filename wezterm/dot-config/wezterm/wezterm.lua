2a4ff6a2007283b697519df96d9193e58ee60b2d
Xavier Valarino <xvalarino@gmail.com>
2025-06-11 19:05:01 -0700

    Wezterm: use more variants of kanso theme

--- wezterm/dot-config/wezterm/wezterm.lua
+++ wezterm/dot-config/wezterm/wezterm.lua
@@ -1,4 +1,6 @@
 local wezterm = require("wezterm")
+local theme = require("kanso-theme")
+
 local config = {}
 
 if wezterm.config_builder then
@@ -16,70 +18,12 @@ config.window_content_alignment = {
 
 config.force_reverse_video_cursor = true
 
-local kanso_ink_colors = {
-    foreground = "#C5C9C7",
-    background = "#14171d",
-
-    cursor_bg = "#C5C9C7",
-    cursor_fg = "#14171d",
-    cursor_border = "#C5C9C7",
-
-    selection_fg = "#C5C9C7",
-    selection_bg = "#393B44",
-
-    scrollbar_thumb = "#393B44",
-    split = "#393B44",
-
-    ansi = {
-        "#14171d",
-        "#C4746E",
-        "#8A9A7B",
-        "#C4B28A",
-        "#8BA4B0",
-        "#A292A3",
-        "#8EA4A2",
-        "#A4A7A4",
-    },
-    brights = {
-        "#A4A7A4",
-        "#E46876",
-        "#87A987",
-        "#E6C384",
-        "#7FB4CA",
-        "#938AA9",
-        "#7AA89F",
-        "#C5C9C7",
-    },
-    tab_bar = {
-        background = "#14171d",
-        active_tab = {
-            bg_color = "#14171d",
-            fg_color = "#7AA89F",
-            intensity = "Bold",
-        },
-
-        inactive_tab = {
-            bg_color = "#14171d",
-            fg_color = "#A4A7A4",
-            intensity = "Half",
-        },
-    },
-}
-
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
 config.show_new_tab_button_in_tab_bar = false
 
-local kanso_ink = {
-    background = "#14171d",
-    foreground = "#C5C9C7",
-    muted = "#6E7370",
-    highlight = "#393B44",
-    faded = "#1A1C22",
-}
-
 -- TODO: set this up so that it switches with term theme
-config.colors = kanso_ink_colors
+config.colors = theme.mist
 
 local default_padding = {
     left = 36,
